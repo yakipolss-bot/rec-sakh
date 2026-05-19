bf11f7d41826295077074c3b0873a01230d9d17b
zaebalsyaya0505-max <zaebalsyaya05@gmail.com>
2026-05-20 08:12:41 +1100

    fix: wrap client hydration in Layout to provide ThemeProvider+FavoritesProvider context

--- app/pages/+onRenderClient.tsx
+++ app/pages/+onRenderClient.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { PageContextProvider } from 'vike-react/usePageContext';
 import { BrowserRouter } from 'react-router-dom';
+import Layout from './+Layout';
 
 export { onRenderClient };
 
@@ -12,7 +13,9 @@ async function onRenderClient(pageContext: any) {
     document.getElementById('root')!,
     <PageContextProvider pageContext={pageContext}>
       <BrowserRouter>
-        <Page {...pageProps} />
+        <Layout>
+          <Page {...pageProps} />
+        </Layout>
       </BrowserRouter>
     </PageContextProvider>,
   );
