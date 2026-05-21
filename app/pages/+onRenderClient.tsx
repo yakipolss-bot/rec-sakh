1ae1fcae1000f50a1cbd13bded24bf831ed7844b
zaebalsyaya0505-max <zaebalsyaya05@gmail.com>
2026-05-22 09:44:52 +1100

    fix: call hydrateRoot only on initial hydration, use root.render() for subsequent navigations to preserve AuthProvider state

--- app/pages/+onRenderClient.tsx
+++ app/pages/+onRenderClient.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
+import { hydrateRoot, createRoot, type Root } from 'react-dom/client';
 import { PageContextProvider } from 'vike-react/usePageContext';
 import type { PageContextClient } from 'vike/types';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,17 +7,24 @@ import Layout from './+Layout';
 
 export { onRenderClient };
 
-async function onRenderClient(pageContext: PageContextClient) {
-  const { Page, pageProps } = pageContext;
+let root: Root | undefined;
 
-  hydrateRoot(
-    document.getElementById('root')!,
+async function onRenderClient(pageContext: PageContextClient) {
+  const { Page, pageProps, isHydration } = pageContext;
+
+  const page = (
     <PageContextProvider pageContext={pageContext}>
       <BrowserRouter>
         <Layout>
           <Page {...pageProps} />
         </Layout>
       </BrowserRouter>
-    </PageContextProvider>,
+    </PageContextProvider>
   );
+
+  if (isHydration) {
+    root = hydrateRoot(document.getElementById('root')!, page);
+  } else {
+    root!.render(page);
+  }
 }
